794fad973e5af3723d38dbee90f7661004ef57c2
Kakadua03 <n.schumacher@students.htl-leonding.ac.at>
2026-01-26 23:19:33 +0100

    commit for ValionCode because of commit problems

--- js/scripts.js
+++ js/scripts.js
@@ -31,4 +31,31 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+        /* =========================
+       Gallery Slider
+       ========================= */
+
+    const galleryImages = [
+        "https://images.unsplash.com/photo-1712095314770-5ea32f8b0505?q=80&w=2670&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1602182244320-12e14faa479f?q=80&w=2670&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1623476950375-368107fffaa9?q=80&w=3870&auto=format&fit=crop",
+        "https://images.unsplash.com/photo-1552962700-b7fcd17d89bf?q=80&w=2728&auto=format&fit=crop",
+        "https://cdn.pixabay.com/photo/2023/10/20/06/51/car-8328373_1280.jpg"
+    ];
+
+    let currentIndex = 0;
+    const galleryImage = document.getElementById("galleryImage");
+
+    if (galleryImage) {
+        window.nextImage = function () {
+            currentIndex = (currentIndex + 1) % galleryImages.length;
+            galleryImage.src = galleryImages[currentIndex];
+        };
+
+        window.prevImage = function () {
+            currentIndex = (currentIndex - 1 + galleryImages.length) % galleryImages.length;
+            galleryImage.src = galleryImages[currentIndex];
+        };
+    }
+
 });
